dc8f939fce49529d01859f68c444d6fcbca5cba1
iamhack249 <53992946+iamhack249@users.noreply.github.com>
2023-03-26 00:26:03 +0700

    Update settings.js

--- Alphabot-Md/settings.js
+++ Alphabot-Md/settings.js
@@ -7,7 +7,7 @@ const {
 } = require(`./language`)
 
 //OpenAi || ChatGpt
-global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
+global.OpenAIKey = "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" //login to https://platform.openai.com/account/api-keys to get apikey
 
 // Website Api (jgn di ganti tomlol)
 global.APIs = {
